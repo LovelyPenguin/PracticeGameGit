3bd9c504c7e9f6e733acef149164ff101d384616
LovelyPenguin <lyp3238@gmail.com>
2019-02-27 10:17:05 +0900

    캔버스로 만들면 안되겠다는걸 발견함

---
+++ Assets/Script/BackgroundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform[] image;
+    private RectTransform temp;
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        for (int i = 0; i < image.Length; i++)
+        {
+            image[i].Translate(Vector3.down * 500 * Time.deltaTime);
+            if (image[0].position.y <= 0)
+            {
+                image[i + 1].localPosition = new Vector3(0, image[0].localPosition.y + 1638, 0);
+                temp = image[i];
+                image[i] = image[i + 1];
+                image[i + 1] = temp;
+            }
+        }
+    }
+}
